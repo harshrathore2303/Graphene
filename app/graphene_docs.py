b3c357f3b544132cc04894bfd04741afa2640291
Harshit Singh Rathore <harsh.rathore.2303@gmail.com>
2024-09-15 20:23:34 +0530

    changed the class for easy understanding

--- app/graphene_docs.py
+++ app/graphene_docs.py
@@ -1,46 +1,56 @@
-from graphene import Schema, ObjectType, String, Int, Field, List, NonNull
+from graphene import ObjectType, Field, String, Schema, NonNull, List
 from json import dumps
 
-student = [
-{
-    "name": "John Doe",
-    "age": 19,
-    "gender": 'Male',
-    "subjects": ['Math', 'Science', 'English']
-},
-{
-    "name": "Harry Potter",
-    "age": 18,
-    "gender": "Male",
-    "subjects": ['Math', 'Science']
+class Human(ObjectType):
+    name = Field(NonNull(String), description="The name of the human")
+    age = Field(NonNull(String), description="The age of the human")
+    gender = Field(NonNull(String), description="The gender of the human")
+    hobbies = Field(List(String), description="The hobbies of the human (nullable list)")
+
+class Query(ObjectType):
+    human_by_name = Field(NonNull(Human), name=NonNull(String))
+
+    def resolve_human_by_name(self, info, name):
+        return get_human(name=name)
+
+
+def get_human(name):
+    humans = [
+        {
+            "name": "John Doe",
+            "age": "30",
+            "gender": "Male",
+            "hobbies": ['Reading', 'Swimming', 'Cycling']
+        },
+        {
+            "name": "Jane Smith",
+            "age": "25",
+            "gender": "Female",
+            "hobbies": None
+        }
+    ]
+    for human in humans:
+        h = dict(**human)
+        print(h)
+        if human["name"] == name:
+            return Human(**human)
+    return None
+
+# Create the schema
+schema = Schema(query=Query)
+
+# Example usage (you can run this part in your Python script to test the schema)
+query = '''
+{ 
+    humanByName(name: "John Doe") 
+    {
+        name 
+        age 
+        gender 
+        hobbies
+    } 
 }
-]
-
-class Student(ObjectType):
-    name = Field(NonNull(String), description="The name of the student")
-    age = Field(NonNull(Int), description="The age of the student")
-    gender = Field(NonNull(String), description="The gender of the student")
-    subjects = Field(List(String), description="The subjects of the student")
-
-    def resolve_name(student, info):
-        return f"{student[0].name}"
-    
-
-
-
-
-my_schema = Schema(
-    query=Student
-)
-
-# print(my_schema)
-
-query_student = '''
-query student {
-    name
-}    
 '''
-result = my_schema.execute(query_student)
+result = schema.execute(query)
 print(dumps(result.data, indent=2))
-# query_string = 'query whoIsMyBestFriend { myBestFriend { lastName } }'
-# my_schema.execute(query_string)
+    
